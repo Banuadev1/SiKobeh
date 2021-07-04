8c3a1019ec55543056addaac969a4a7a5d0aec10
DigitMarsshalAssah09 <tugasdigit@gmail.com>
2021-07-04 23:43:38 +0800

    Pembaruan tampilan CekLaporan, next compe duls

--- app/src/main/java/com/example/sikobeh/WartawanAdapter.java
+++ app/src/main/java/com/example/sikobeh/WartawanAdapter.java
@@ -5,17 +5,27 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class WartawanAdapter extends RecyclerView.Adapter<WartawanAdapter.MyViewHolder>  {
 
     Context context;
     ArrayList<User> list;
+    FirebaseAuth fAuth;
+    StorageReference storageReference;
 
     public WartawanAdapter(Context context, ArrayList<User> list) {
         this.context = context;
@@ -35,9 +45,15 @@ public class WartawanAdapter extends RecyclerView.Adapter<WartawanAdapter.MyView
         holder.fullname.setText(user.getFullname());
         holder.email.setText(user.getEmail());
         holder.pnumber.setText(user.getPnumber());
+        String value = user.getUid();
 
-        holder.fullname.setOnClickListener(v -> {
-            String value = user.getUid();
+        fAuth = FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        StorageReference profileRef = storageReference.child("users/"+value+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(uri -> Picasso.get().load(uri).into(holder.bimage));
+
+        holder.cardView.setOnClickListener(v -> {
             Intent intent = new Intent(context, CekLaporan2.class);
             intent.putExtra("key", value);
             context.startActivity(intent);
@@ -51,7 +67,9 @@ public class WartawanAdapter extends RecyclerView.Adapter<WartawanAdapter.MyView
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
-        TextView fullname, email, pnumber, uid;
+        TextView fullname, email, pnumber;
+        CircleImageView bimage;
+        RelativeLayout cardView;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -59,6 +77,8 @@ public class WartawanAdapter extends RecyclerView.Adapter<WartawanAdapter.MyView
             fullname = (TextView) itemView.findViewById(R.id.fullname);
             email = (TextView) itemView.findViewById(R.id.email);
             pnumber = (TextView) itemView.findViewById(R.id.pnumber);
+            bimage = (CircleImageView) itemView.findViewById(R.id.beritaImage);
+            cardView = (RelativeLayout) itemView.findViewById(R.id.areaklik);
 
             fullname.setOnClickListener(new View.OnClickListener() {
                 @Override
