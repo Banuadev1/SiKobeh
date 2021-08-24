822c393fc6100a333d0aa266bead3c5fe32bd7df
DigitMarsshalAssah09 <tugasdigit@gmail.com>
2021-08-24 16:38:50 +0800

    Mengunduh dalam bentuk text, dan dapat juga mengirim melalui whatsapp

--- app/src/main/java/com/example/sikobeh/DataBeritaAdapter.java
+++ app/src/main/java/com/example/sikobeh/DataBeritaAdapter.java
@@ -5,10 +5,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -34,23 +37,22 @@ import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-
-import org.apache.poi.util.Units;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class DataBeritaAdapter extends FirebaseRecyclerAdapter<Berita, DataBeritaAdapter.MyViewHolder> {
     public static final int GALLERY_REQUEST_CODE = 105;
     Context context;
     Bitmap bitmap;
+    private SharedPreferences phoneNumber;
     private File filePath = null, folder = null, gambar = null;
+    String auth = FirebaseAuth.getInstance().getCurrentUser().getUid(), catchError;
 
     /**
      * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
@@ -61,6 +63,7 @@ public class DataBeritaAdapter extends FirebaseRecyclerAdapter<Berita, DataBerit
     public DataBeritaAdapter(Context context, @NonNull FirebaseRecyclerOptions<Berita> options) {
         super(options);
         this.context = context;
+        phoneNumber = context.getSharedPreferences("phoneNumber", MODE_PRIVATE);
     }
 
     @Override
@@ -93,43 +96,40 @@ public class DataBeritaAdapter extends FirebaseRecyclerAdapter<Berita, DataBerit
                 }
             })
             .error(R.drawable.ic_baseline_image_24_berita).into(holder.fotoBerita);
-        holder.buatDokumenB.setOnClickListener(v -> {
+        holder.pilihanB.setOnClickListener(v -> {
+            bitmap = ((BitmapDrawable)holder.fotoBerita.getDrawable()).getBitmap();
             ActivityCompat.requestPermissions((Activity) context, new String[]{
                     Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE
             }, PackageManager.PERMISSION_GRANTED);
-            final View customLayout2 = holder.li.inflate(R.layout.edit_nama_file, null);
+            final View customLayout2 = holder.li.inflate(R.layout.pilihan_lanjutan_berita, null);
             AlertDialog.Builder builder2 = new AlertDialog.Builder(context);
             builder2.setView(customLayout2);
-            builder2.setTitle("Nama File Dokumen (*.docx)");
+            builder2.setTitle("Pilihan Lanjutan");
 
-            EditText edNamaFile = customLayout2.findViewById(R.id.edNamaDokumen);
-            edNamaFile.setText("Berita-"+model.getJudul());
-            builder2.setPositiveButton("Simpan", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    bitmap = ((BitmapDrawable)holder.fotoBerita.getDrawable()).getBitmap();
-                    folder = new File(Environment.getExternalStorageDirectory() + File.separator + "Sikobeh");
-                    if (!folder.exists()){
-                        folder.mkdirs();
-                    }
-                    String str = edNamaFile.getText().toString();
-                    if (str.isEmpty()){
-                        edNamaFile.setError("Nama file harus diisi");
-                    } else {
-                        if (prepareDocx(str)) {
-                            getImageURL();
-                            String[] data = {model.getJudul(), model.getLoc(), model.getDesc(), model.getTimeupload(), model.getBeritaurl()};
-                            createDocx(data /*, holder.fotoBerita */);
-                        } else {
-                            Toast.makeText(context, "Nama file sudah terpakai!", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                }
+            Button bagikanWAB = customLayout2.findViewById(R.id.bagikanWA);
+            Button buatFileTextB = customLayout2.findViewById(R.id.buatFileText);
+            String[] data = {model.getJudul(), model.getLoc(), model.getDesc(), model.getTimeupload()};
+
+            folder = new File(Environment.getExternalStorageDirectory() + File.separator +
+                    "Sikobeh");
+            if (!folder.exists()){
+                folder.mkdirs();
+            }
+
+            bagikanWAB.setOnClickListener(v1 -> {
+                bagikanKeWA(holder.li, data);
             });
-            builder2.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
+            buatFileTextB.setOnClickListener(v1 -> {
+                folder = new File(Environment.getExternalStorageDirectory() + File.separator +
+                        "Sikobeh" + File.separator + "Text File");
+                if (!folder.exists()){
+                    if (folder.mkdirs()){
+                        simpanFileText(data, holder.li);
+                    } else {
+                        Toast.makeText(context, "Terjadi kesalahan saat membuat folder Sikobeh", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    simpanFileText(data, holder.li);
                 }
             });
             AlertDialog dialog1 = builder2.create();
@@ -147,7 +147,8 @@ public class DataBeritaAdapter extends FirebaseRecyclerAdapter<Berita, DataBerit
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         TextView judul, deskripsi, lokasi, tanggal;
-        Button ubahB, buatDokumenB;
+        Button ubahB;
+        ImageButton pilihanB;
         ImageView fotoBerita;
         ProgressBar progressBar;
         LayoutInflater li;
@@ -158,7 +159,7 @@ public class DataBeritaAdapter extends FirebaseRecyclerAdapter<Berita, DataBerit
             judul = itemView.findViewById(R.id.judultext);
             deskripsi = itemView.findViewById(R.id.descberita);
             ubahB = itemView.findViewById(R.id.ubahBerita);
-            buatDokumenB = itemView.findViewById(R.id.buatDokumen);
+            pilihanB = itemView.findViewById(R.id.pilihan);
             lokasi = itemView.findViewById(R.id.locberita);
             fotoBerita = itemView.findViewById(R.id.fotoberita);
             tanggal = itemView.findViewById(R.id.tanggal);
@@ -166,81 +167,180 @@ public class DataBeritaAdapter extends FirebaseRecyclerAdapter<Berita, DataBerit
         }
     }
 
-    private void getImageURL(){
+    private boolean cekWAInstalled(){
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            packageManager.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e){
+            return false;
+        }
+    }
+
+    private String convertToCodeNumber(String pn){
+        String str = pn;
+        if (pn.startsWith("0")){
+            str = "62" + pn.substring(1, str.length());
+        }
+        return str;
+    }
+
+    private String convertToTextWa(String[] data){
+        String s = "*Aplikasi SiKobeh*" + "%0a%0a" +
+                "Judul :%0a" + data[0] + "%0a%0a" +
+                "Lokasi :%0a" + data[1] + "%0a%0a" +
+                "Deskripsi :%0a" + data[2] + "%0a%0a" +
+                "Waktu Mengunggah :%0a" + data[3];
+        return s;
+    }
+
+    private void bagikanKeWA(LayoutInflater li, String[] dataBerita){
+        ActivityCompat.requestPermissions((Activity) context, new String[]{
+                Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        }, PackageManager.PERMISSION_GRANTED);
+        final View customLayout2 = li.inflate(R.layout.edit_nomor_wa, null);
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(context);
+        builder2.setView(customLayout2);
+        builder2.setTitle("Masukkan nomor telepon Anda");
+
+        EditText edNP = customLayout2.findViewById(R.id.edNomorTelp);
+        String phone = phoneNumber.getString(auth, "false");
+        if (!phone.equals("false")){
+            edNP.setText(phone);
+        }
+        builder2.setPositiveButton("Kirim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String data = edNP.getText().toString();
+                phoneNumber.edit().putString(auth, data).apply();
+                if (!data.isEmpty()){
+                    if (cekWAInstalled()){
+                        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                                "https://api.whatsapp.com/send?phone="+convertToCodeNumber(data)+
+                                        "&text="+convertToTextWa(dataBerita)));
+                        context.startActivity(i);
+                        edNP.getText().clear();
+                    } else {
+                        Toast.makeText(context, "Whatsapp tidak terinstal", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(context, "Masukkan nomor telepon terlebih dahulu", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder2.setNegativeButton("Kembali", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog dialog1 = builder2.create();
+        dialog1.getWindow().setLayout(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        dialog1.show();
+    }
+
+    private boolean prepareFolder(String str){
+        File env = Environment.getExternalStorageDirectory();
+        // can switch with this : getExternalFilesDir(null) -> its gonna save to com.example.sikobeh
+        filePath = new File(env + File.separator + "Sikobeh" + File.separator + "Text File" +
+                File.separator + str, str+".txt");
+        gambar = new File(env + File.separator + "Sikobeh" + File.separator + "Text File" +
+                File.separator + str, str+".JPEG");
+        try {
+            if (!gambar.exists()){
+                gambar.createNewFile();
+            }
+            if (!filePath.exists()){
+                filePath.createNewFile();
+            }
+            return true;
+        } catch (IOException e){
+            e.printStackTrace();
+            catchError = e.toString();
+            return false;
+        }
+    }
+
+    private void simpanFileText(String[] data, LayoutInflater li){
+        ActivityCompat.requestPermissions((Activity) context, new String[]{
+                Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        }, PackageManager.PERMISSION_GRANTED);
+        final View customLayout2 = li.inflate(R.layout.edit_nama_file, null);
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(context);
+        builder2.setView(customLayout2);
+        builder2.setTitle("Nama File Teks (*.txt)");
+
+        EditText edNamaFile = customLayout2.findViewById(R.id.edNamaDokumen);
+        edNamaFile.setText("Berita-"+data[0]);
+        builder2.setPositiveButton("Simpan", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String edT = edNamaFile.getText().toString();
+                if (makeDirTextBerita(edT)){
+                    if (prepareFolder(edT)){
+                        writeFile(data);
+                    } else {
+                        //Toast.makeText(context, catchError, Toast.LENGTH_LONG).show();
+                        Toast.makeText(context, "Terjadi kesalahan saat penentuan penyimpanan", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(context, "Terjadi kesalahan saat membuat penyimpanan File Text", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder2.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog dialog1 = builder2.create();
+        dialog1.getWindow().setLayout(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        dialog1.show();
+    }
+
+    private String convertToFileText(String[] data){
+        String s = "*Aplikasi SiKobeh*" + "\n\n" +
+                "Judul :\n" + data[0] + "\n\n" +
+                "Lokasi :\n" + data[1] + "\n\n" +
+                "Deskripsi :\n" + data[2] + "\n\n" +
+                "Waktu Mengunggah :\n" + data[3];
+        return s;
+    }
+
+    private void writeFile(String[] data){
+        try {
+            FileOutputStream fos = new FileOutputStream(filePath);
+            fos.write(convertToFileText(data).getBytes());
+            fos.close();
+            simpanFoto();
+        } catch (IOException e) {
+            Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void simpanFoto(){
         try {
             OutputStream fo = new FileOutputStream(gambar);
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fo);
             fo.flush();
             fo.close();
-            Toast.makeText(context, "Berhasil", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "File tersimpan di "+folder.toString(), Toast.LENGTH_SHORT).show();
         } catch (IOException ex) {
             Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 
-    private Boolean prepareDocx(String str){
-        // parameter_1 can switch with this : getExternalFilesDir(null) -> its gonna save to com.example.gudangsederhana
-        filePath = new File(Environment.getExternalStorageDirectory()+File.separator+"Sikobeh", str+".docx");
-        gambar = new File(Environment.getExternalStorageDirectory()+File.separator+"Sikobeh", "Berita.JPEG");
-
-        try {
-            if (!filePath.exists()){
-                filePath.createNewFile();
-                gambar.createNewFile();
+    private Boolean makeDirTextBerita(String str){
+        folder = new File(Environment.getExternalStorageDirectory() + File.separator +
+                "Sikobeh" + File.separator + "Text File" + File.separator + str);
+        if (!folder.exists()){
+            if (folder.mkdirs()){
                 return true;
             } else {
                 return false;
             }
-        } catch (IOException e){
-            e.printStackTrace();
-            return false;
         }
-    }
-
-    private void createDocx(String[] data){
-        try {
-            XWPFDocument xwpfDocument = new XWPFDocument();
-            XWPFParagraph xwpfParagraph = xwpfDocument.createParagraph();
-            xwpfParagraph.setSpacingAfterLines(1);
-            XWPFRun xwpfRun = xwpfParagraph.createRun();
-
-            xwpfRun.setFontSize(12);
-            xwpfRun.setFontFamily("Times New Roman");
-
-            xwpfRun.setBold(true);
-            xwpfRun.setText("Sistem Informasi Konsep Berita Harian (SiKobeh");
-            xwpfRun.addBreak(); xwpfRun.addBreak();
-            xwpfRun.setBold(false);
-
-            xwpfRun.setText("Dari : " + WartawanForm.Fname);
-            xwpfRun.addBreak(); xwpfRun.addBreak();
-
-            xwpfRun.setText("Judul : " + data[0]); xwpfRun.addBreak();
-            xwpfRun.setText("Lokasi : " + data[1]); xwpfRun.addBreak(); // xwpfRun.addTab();
-            xwpfRun.setText("Deskripsi : " + data[2]); xwpfRun.addBreak();
-            xwpfRun.setText("Waktu Penyetoran : " + data[3]); xwpfRun.addBreak();
-            FileInputStream is = new FileInputStream(gambar);
-//            byte[] bytes = new byte[4];
-//            is.read(bytes);
-//            Double width = new BigInteger(bytes).doubleValue();
-//            is.read(bytes);
-//            Double height = new BigInteger(bytes).doubleValue();
-//            Double cWidth = width/(height/300);
-            xwpfRun.addPicture(is, XWPFDocument.PICTURE_TYPE_JPEG, "berita.JPEG", Units.toEMU(300), Units.toEMU(300)); // 300x300 pixels
-            is.close();
-
-            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
-            xwpfDocument.write(fileOutputStream);
-
-            if (fileOutputStream != null){
-                fileOutputStream.flush();
-                fileOutputStream.close();
-                String textT = "Berhasil menyimpan dokumen (*.docx)";
-                Toast.makeText(context, textT, Toast.LENGTH_LONG).show();
-            }
-            xwpfDocument.close();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        return true;
     }
 }
